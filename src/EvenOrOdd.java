9abfbcf55c54fb8a6f6e10ae6a97dc9c75258d4d
Rahul Sahani <yourahul1917@gmail.com>
2023-02-17 23:33:20 +0530

    updates !!.

--- src/EvenOrOdd.java
+++ src/EvenOrOdd.java
@@ -2,10 +2,17 @@ import org.w3c.dom.ls.LSOutput;
 
 import java.util.*;
 public class EvenOrOdd {
-    //Write a program to find number entered by the user is odd or even
-    Scanner input= new Scanner(System.in);
-    System.out.println("Enter a number : ");
-    int n = input.nextInt();
-    if(n%2==0){System.out.println("The number entered is even");}
-    else{System.out.println("The number entered is odd");}
+
+    public static void main(String[] args) {
+        //Write a program to find number entered by the user is odd or even
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = input.nextInt();
+        if (n % 2 == 0) {
+            System.out.println("The number entered is even");
+        } else {
+            System.out.println("The number entered is odd");
+        }
+    }
 }
+
